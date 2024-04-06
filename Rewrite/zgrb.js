823ffa713dd61c7d53f630f037584568a00c25f3
sunxihe <108460355+sunxihe@users.noreply.github.com>
2024-04-06 22:42:22 +0800

    Update zgrb.js

--- Rewrite/zgrb.js
+++ Rewrite/zgrb.js
@@ -3,12 +3,11 @@ const sxh = init()
 
 const bark_switch = sxh.getdata('bark_switch')
 const database_switch = sxh.getdata('database_switch')
-let zgrb_time;
-if (sxh.getdata('zgrb_time') == "(null)") {
+if (sxh.getdata('zgrb_time') == "") {
     sxh.setdata("1970/01/01", 'zgrb_time');
-} else {
-    zgrb_time = sxh.getdata('zgrb_time');
 }
+zgrb_time = sxh.getdata('zgrb_time');
+
 const now_time = formatCurrentDate()
 
 if ($request  && $request.url.indexOf('appUserLoginInfo') >= 0) {
