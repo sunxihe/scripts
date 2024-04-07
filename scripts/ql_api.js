982290cce1f4654ed8bb823488f4e727a7e9cbca
sunxihe <108460355+sunxihe@users.noreply.github.com>
2024-04-07 21:28:42 +0800

    Update ql_api.js

--- scripts/ql_api.js
+++ scripts/ql_api.js
@@ -11,7 +11,7 @@ $.ql = {
         const opt = {
 			url: `${$.ql_url}/${this.type}/open/crons/run`,
 			headers: this.headers,
-			body: ids,
+			body: JSON.stringify(ids),
 		};
         $.log(opt);
         return $.http.put(opt).then((response) => JSON.parse(response.body));
